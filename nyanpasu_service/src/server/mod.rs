e92081fa8d4fe51d69bfb1278cb344391a988251
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2025-02-21 00:11:38 +0800

    fix: linter

--- nyanpasu_service/src/server/mod.rs
+++ nyanpasu_service/src/server/mod.rs
@@ -3,13 +3,13 @@ mod instance;
 mod logger;
 mod routing;
 
+use axum::Router;
 pub use instance::CoreManagerWrapper as CoreManager;
 pub use logger::Logger;
 use nyanpasu_ipc::{SERVICE_PLACEHOLDER, server::create_server};
 use routing::apply_routes;
 use tokio_util::sync::CancellationToken;
 use tracing_attributes::instrument;
-use axum::Router;
 
 #[instrument]
 // TODO: impl axum graceful shutdown, and wrap inner stream into axum trait
